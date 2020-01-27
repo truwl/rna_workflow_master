34609c561e464adfe9d5ff494413577adf9ca719
Brad <basickler@gmail.com>
2020-01-26 22:28:06 -0800

    fix for pull, mkdir

--- pull_cromwell.sh
+++ pull_cromwell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 OUT_DIR="opt/cromwell"
+mkdir -p ${OUT_DIR}
 wget -O "${OUT_DIR}/cromwell.jar" https://github.com/broadinstitute/cromwell/releases/download/48/cromwell-48.jar
 wget -O "${OUT_DIR}/womtool.jar" https://github.com/broadinstitute/cromwell/releases/download/48/womtool-48.jar
